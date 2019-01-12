e335d8b539e7c309ba551aba5425fc343005c1a7
KavithaEV <kavithavarughese@gmail.com>
2019-01-12 18:04:44 +0530

    Setting up Camera so that it sticks to the player at all times at a particular distance

--- Assets/_Complete-Game/Scripts/Camera/CameraFollow.cs
+++ Assets/_Complete-Game/Scripts/Camera/CameraFollow.cs
@@ -6,7 +6,7 @@ namespace CompleteProject
     public class CameraFollow : MonoBehaviour
     {
         public Transform target;            // The position that that camera will be following.
-        public float smoothing = 5f;        // The speed with which the camera will be following.
+        public float smoothing = 5f;        // The speed with which the camera will be following. ;giving a smoothing effect 
 
 
         Vector3 offset;                     // The initial offset from the target.
@@ -15,17 +15,18 @@ namespace CompleteProject
         void Start ()
         {
             // Calculate the initial offset.
-            offset = transform.position - target.position;
+            offset = transform.position - target.position;  //camera position - player position
         }
 
 
         void FixedUpdate ()
         {
             // Create a postion the camera is aiming for based on the offset from the target.
-            Vector3 targetCamPos = target.position + offset;
+            Vector3 targetCamPos = target.position + offset; //so camera moves in such a way that target and camera stays at same distance always
 
             // Smoothly interpolate between the camera's current position and it's target position.
             transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+            //note : transform will be of that object to which this script is attached
         }
     }
 }
